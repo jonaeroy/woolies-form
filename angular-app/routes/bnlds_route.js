1411996852a57d721a7674bf2aae375b4f893bea
byrenx <jofel.bayron@cloudsherpas.com>
2015-02-11 21:48:32 +0800

    fixed include.yaml error

--- angular-app/routes/bnlds_route.js
+++ angular-app/routes/bnlds_route.js
@@ -2,10 +2,10 @@ App.config(['$routeProvider',
 	    function($routeProvider){
 		$routeProvider.
 		    when('/add_form', {
-			templateUrl: '/ng/bnlds/bnlds_form.html'
+			templateUrl: '/ngtemplates/bnlds/bnlds_form.html'
 		    }).
 		    when('/bnlds_list', {
-			templateUrl: '/ng/bnlds/bnlds_list.html',
+			templateUrl: '/ng/templates/bnlds/bnlds_list.html',
 			controller : 'BnldsList'
 		    }).
 		    otherwise({
