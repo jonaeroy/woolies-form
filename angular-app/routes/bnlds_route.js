bb8c229d0ec764e810392eded65692d1f0765036
jonaeroy <jona.eroy@cloudsherpas.com>
2015-02-12 22:48:38 +0800

    update on bnldsroute and bnldform

--- angular-app/routes/bnlds_route.js
+++ angular-app/routes/bnlds_route.js
@@ -2,7 +2,7 @@ App.config(['$routeProvider',
 	    function($routeProvider){
 		$routeProvider.
 		    when('/add_form', {
-			templateUrl: '/ngtemplates/bnlds/bnlds_form.html'
+			templateUrl: '/ng/templates/bnlds/bnlds_form.html'
 		    }).
 		    when('/bnlds_list', {
 			templateUrl: '/ng/templates/bnlds/bnlds_list.html',
