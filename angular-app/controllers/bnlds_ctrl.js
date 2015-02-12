57c451ef03cf46085df825a0003c4b4db01c28f7
jonaeroy <jona.eroy@cloudsherpas.com>
2015-02-12 22:08:23 +0800

    bnlds

--- angular-app/controllers/bnlds_ctrl.js
+++ angular-app/controllers/bnlds_ctrl.js
@@ -1,7 +1,11 @@
 angular.module('app.controllers').controller('BnldsCtrl', function($scope, BnldsSvc){
     "use strict";
+    //variables
 
     $scope.bnlds = {};
+    //select options variables
+    $scope.choices = ["Yes", "No","N/A"];
+    //ng-options = "choice for choice in choices track by choice" ng-model="default_choice"
     $scope.create = function() {
 	BnldsSvc.create($scope.bnlds)
 	    .success(function(data, status){
